4a85d08379fd78c612ef03e27118fb0bc812f3ba
Jeff McCune <jeff@puppetlabs.com>
2014-11-18 14:19:52 -0800

    (NETDEV-26) Add network_dns type
    Without this patch there is no type to manage the DNS configuration of a
    target device.  This patch addresses the problem by adding the
    network_dns type with a namevar of `domain` and array properties of
    `search` and `servers`.  The type is ensurable because DNS may not be
    configured on the target system.

---
+++ spec/unit/puppet/type/network_dns_spec.rb
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe Puppet::Type.type(:network_dns) do
+  let(:catalog) { Puppet::Resource::Catalog.new }
+  let(:type) { described_class.new(name: 'emanon', catalog: catalog) }
+  subject { described_class.attrclass(attribute) }
+
+  it_behaves_like 'an ensurable type'
+  it_behaves_like 'the namevar is', :domain
+  it_behaves_like 'it has an array property', :servers
+  it_behaves_like 'it has an array property', :search
+end
